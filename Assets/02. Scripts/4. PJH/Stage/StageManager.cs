49d5398c4c7db0f59f21587fae33f05f059c11b6
naneo10 <144968440+naneo10@users.noreply.github.com>
2026-02-02 01:44:24 +0900

    Stage update
    StageConfig.cs delete,
    StageDatabase.cs feature extra, (float -> int) modify

--- Assets/02. Scripts/4. PJH/Stage/StageManager.cs
+++ Assets/02. Scripts/4. PJH/Stage/StageManager.cs
@@ -1,19 +1,42 @@
 using UnityEngine;
 
 /*
+    !해당 스테이지에 해당하는 데이터 연결
+
     *싱글톤
     StageFlowManager 연동
-    StageConfig 메서드 실행
+    StageDatabase 메서드 실행
 */
 public class StageManager : MonoBehaviour
 {
+    #region field
+    public static StageManager Instance { get; private set; }
+    public StageDatabase stageDB;
+    #endregion
+
     void Awake()
     {
-        
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else Destroy(gameObject);
     }
 
     void Update()
     {
         
     }
+
+    #region method
+    public void StageData(int id)
+    {
+        StageConfigSO data = stageDB.GetStageData(id);
+        if (data != null)
+        {
+            //해당 스테이지 데이터를 불러오기
+        }
+    }
+    #endregion
 }
