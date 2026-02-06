d933a8a38f534f70592e8eab46e89a5e4efc6396
태완 <ckxodhks1234@gmail.com>
2026-02-06 13:26:29 +0900

    TW_0206Commit

--- Assets/02. Scripts/4. PJH/ObjectPool/EnemyFactory.cs
+++ Assets/02. Scripts/4. PJH/ObjectPool/EnemyFactory.cs
@@ -6,6 +6,11 @@ public class EnemyFactory : MonoBehaviour
 
     public GameObject Create(EnemyConfigSO enemySO, Vector3 position, Quaternion rotation)
     {
+        if (objectPoolManager == null)
+        {
+            Debug.LogError("ObjectPoolManager가 EnemyFactory에 연결 안 됨");
+        }
+
         GameObject enemy = objectPoolManager.SpawnPool(enemySO.monsterPrefab, position, rotation);
         return enemy;
     }
--- Assets/02. Scripts/4. PJH/ObjectPool/EnemySpawnManager.cs
+++ Assets/02. Scripts/4. PJH/ObjectPool/EnemySpawnManager.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class EnemySpawnManager : MonoBehaviour
 {
@@ -7,13 +8,30 @@ public class EnemySpawnManager : MonoBehaviour
     [SerializeField] private StageConfigSO stageSO;
 
     [Header("설정")]
-    [SerializeField] private Transform spawnPoint;
-    [SerializeField] private int spawnCount;
+    [SerializeField] private Transform player;
+    [SerializeField] private float spawnDistance = 5f;
+    [SerializeField] private LayerMask groundLayer;
 
     //몬스터 수 관련은 몬스터매니저 같은 곳에서 하는 것이 좋을듯함
     private int monsterCount;
 
     private int currentRound = 0;
+
+    //스폰 방향
+    private Vector3[] spawnDirections =
+    {
+        Vector3.forward, Vector3.back, Vector3.left, Vector3.right, //상하좌우
+        Vector3.forward + Vector3.left, Vector3.forward + Vector3.right, //좌상, 우상
+        Vector3.back + Vector3.left, Vector3.back + Vector3.right //좌하, 우하
+    };
+
+    private void Awake()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+        }
+    }
 
     void Start()
     {
@@ -24,14 +42,41 @@ public class EnemySpawnManager : MonoBehaviour
 
     private void Spawn()
     {
-        var roundData = stageSO.roundDatas[currentRound];
+        if (currentRound >= stageSO.roundDatas.Count) return;
+
+        var roundData = stageSO.roundDatas[currentRound]; //현재라운드 SO
+        int dirCount = spawnDirections.Length; //방향갯수
+        int dirIndex = 0; //방향 인덱스
+
         //라운드데이터 리스트만큼 몬스터 생성
         foreach (var monster in roundData.monsterSpawnList)
         {
-            enemyFactory.Create(monster, spawnPoint.position, Quaternion.identity);
+            Vector3 dir = spawnDirections[dirIndex];
+            Vector3 spawnPos = player.position + dir * spawnDistance;
+            Vector3 finalSpawnPos = OnGround(spawnPos); //땅이 굴곡져도 땅 위에 있게하기
+            enemyFactory.Create(monster, finalSpawnPos, Quaternion.identity);
 
             monsterCount++;
+            dirIndex++; //방향 인덱스 추가로 다음 스폰 방향 가져오기
+
+            //만약 8마리 이상일 때 처음부터 다시
+            if(dirIndex >= dirCount)
+            {
+                dirIndex = 0;
+            }
         }
+    }
+
+    private Vector3 OnGround(Vector3 groundPos)
+    {
+        Vector3 ray = groundPos + Vector3.up * 5f; //5f 위에서 레이쏘기
+
+        //레이를 아래로 쏴서 맞은 벡터값 리턴하기
+        if(Physics.Raycast(ray, Vector3.down, out RaycastHit hit, 7f, groundLayer))
+        {
+            return hit.point;
+        }
+        return groundPos;
     }
 
     private void ReSpawn()
