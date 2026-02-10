c7c2db480fcebe7d0e1e32c2dadd1313080ce33d
태완 <ckxodhks1234@gmail.com>
2026-02-10 17:04:29 +0900

    TW_0210Commit

--- Assets/02. Scripts/3. CTW/CameraFollow.cs
+++ Assets/02. Scripts/3. CTW/CameraFollow.cs
@@ -4,20 +4,26 @@ public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private float followSpeed = 5f;
-    [SerializeField] private float maxXDistance;
+    [SerializeField] private float maxXDistance = 10f;
     [SerializeField] private float maxZDistance;
+
+    private float startX;
+    private float startZ;
 
     private void Awake()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        startX = player.position.x;
+        startZ = player.position.z;
     }
 
     private void LateUpdate()
     {
         Vector3 cameraPosition = player.position;
 
-        cameraPosition.x = Mathf.Clamp(cameraPosition.x, -maxXDistance, maxXDistance);
-        cameraPosition.z = Mathf.Clamp(cameraPosition.z, -maxZDistance, maxZDistance);
+        cameraPosition.x = Mathf.Clamp(cameraPosition.x, startX - maxXDistance, startX+maxXDistance);
+        cameraPosition.z = Mathf.Clamp(cameraPosition.z, startZ - maxZDistance, startZ+maxZDistance);
         cameraPosition.y = 25f;
 
         transform.position = cameraPosition;
