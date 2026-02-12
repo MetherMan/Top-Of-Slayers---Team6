c99af2ce7b9129ba7996c88c711f4f8ece0722d2
naneo10 <144968440+naneo10@users.noreply.github.com>
2026-02-12 17:28:46 +0900

    0212 1st update
    Addressable name modify -> simply name feature
    AddressableManager modify feature extra
    StageConfigSO modify - AssetReference add

--- Assets/02. Scripts/4. PJH/AddressableManager.cs
+++ Assets/02. Scripts/4. PJH/AddressableManager.cs
@@ -1,36 +1,101 @@
 ﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.AddressableAssets;
 using UnityEngine.ResourceManagement.AsyncOperations;
+using UnityEngine.ResourceManagement.ResourceProviders;
 
 public class AddressableManager : Singleton<AddressableManager>
 {
-    private Dictionary<string, Object> assetCache = new Dictionary<string, Object>();
+    #region field
+    private Dictionary<string, StageConfigSO> _stageDB = new Dictionary<string, StageConfigSO>();
+    private Dictionary<string, SceneInstance> _scene = new Dictionary<string, SceneInstance>();
+    #endregion
 
     protected override void Awake()
     {
         base.Awake();
     }
 
-    public void LoadAsset<T>(string address, System.Action<T> onComplete) where T : Object
+    #region method
+    //유효성 && 완료 && 성공 체크 메서드
+    private bool IsSucceeded(AsyncOperationHandle handle)
     {
-        if (assetCache.ContainsKey(address))
+        if (handle.IsValid() && handle.IsDone && handle.Status == AsyncOperationStatus.Succeeded)
         {
-            onComplete?.Invoke(assetCache[address] as T);
-            return;
+            return true;
+        }
+        return false;
+    }
+
+    private bool IsFailed(AsyncOperationHandle handle)
+    {
+        if (!handle.IsValid())
+        {
+            Debug.LogError("유효성 검사 실패");
+            return true;
         }
 
-        Addressables.LoadAssetAsync<T>(address).Completed += handle =>
+        if (handle.Status == AsyncOperationStatus.Failed)
         {
-            if (handle.Status == AsyncOperationStatus.Succeeded)
-            {
-                assetCache[address] = handle.Result;
-                onComplete?.Invoke(handle.Result);
-            }
-            else
-            {
-                Debug.LogError($"로드 실패: {address}");
-            }
-        };
+            Debug.LogError("Status.Failed");
+            return true;
+        }
+        return false;
     }
+    #region Load
+    //StageConfigSO
+    public async Task LoadAllStageDB()
+    {
+        AsyncOperationHandle<IList<StageConfigSO>> handle 
+            = Addressables.LoadAssetsAsync<StageConfigSO>("StageSO", null);
+
+        await handle.Task;
+
+        if (IsSucceeded(handle))
+        {
+            foreach (StageConfigSO data in handle.Result)
+            {
+                //중복 키 방지
+                if (!_stageDB.ContainsKey(data.stageKey.ToString()))
+                {
+                    _stageDB.Add(data.stageKey.ToString(), data);
+                }
+            }
+        }
+        else if (IsFailed(handle))
+        {
+            Debug.LogError("LoadAllStageDB : Failed");
+            return;
+        }
+    }
+
+    //SceneInstance
+    //LoadAssetsAsync<T> 사용할 경우 메모리 부담
+    public void test()
+    {
+        
+    }
+
+    //RuleSO
+    //MonsterSO
+    //MonsterPrefab
+    //ItemSO
+    //ItemPrefab
+    //VFX
+    //SFX
+    #endregion
+
+    #region Get
+    //stageNum T int -> string으로 형변환
+    public StageConfigSO GetData(string stageNum)
+    {
+        if (_stageDB.TryGetValue(stageNum, out StageConfigSO data))
+        {
+            return data;
+        }
+        return null;
+    }
+    #endregion
+    #endregion
 }
