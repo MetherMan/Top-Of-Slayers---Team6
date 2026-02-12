0e48e7e849a4a7d494bf9e794f18a24268fb2803
태완 <ckxodhks1234@gmail.com>
2026-02-12 17:48:45 +0900

    TW_0212Commit

---
+++ Assets/02. Scripts/3. CTW/Enemy/EnemyAnim.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class EnemyAnim : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void EnemyRunning(bool isRunning)
+    {
+        anim.SetBool("isRunning", isRunning);
+    }
+
+    public void EnemyAttack(bool isAttacking)
+    {
+        anim.SetBool("isAttacking", isAttacking);
+    }
+
+    public void EnemyDie()
+    {
+        anim.SetTrigger("Die");
+    }
+
+    public void EnemyHitted()
+    {
+        anim.SetTrigger("Hit");
+    }
+}
---
+++ Assets/02. Scripts/3. CTW/Enemy/EnemyHitted.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class EnemyHitted : IEnemyState
+{
+    private EnemyBase enemy;
+    private EnemyStateMachine enemyStateMachine;
+
+    private float hitTime = 0.5f;
+    private float timer;
+    public EnemyHitted(EnemyBase enemy, EnemyStateMachine enemyStateMachine)
+    {
+        this.enemy = enemy;
+        this.enemyStateMachine = enemyStateMachine;
+    }
+
+    public void Enter()
+    {
+        timer = hitTime;
+
+        enemy.enemyAnim.EnemyHitted();
+
+        enemy.enemyAnim.EnemyRunning(false);
+        enemy.enemyAnim.EnemyAttack(false);
+    }
+
+    public void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer <= 0)
+        {
+            enemy.enemyStateMachine.ChangeState(enemy.FollowState);
+        }
+    }
+
+    public void Exit()
+    {
+
+    }
+}
